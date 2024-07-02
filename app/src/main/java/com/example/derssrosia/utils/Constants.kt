c9e0c6a235881600a0385618406b91fe19385343
pasangeek <pasan.madusanka9@gmail.com>
2024-07-02 20:23:24 +0900

    package hierarchy added and

---
+++ app/src/main/java/com/example/derssrosia/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.derssrosia.utils
+
+class Constants {
+    companion object {
+
+        const val BASE_URL = " https://fakestoreapi.com/"
+        const val List_Of_product = "products"
+    }
+}
